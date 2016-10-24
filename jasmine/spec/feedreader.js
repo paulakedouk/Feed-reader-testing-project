72d6d848a676cdec69c57136c1d7cce5b405735c
Paula Kedouk <paulakedouk@gmail.com>
2016-10-24 14:08:55 -0700

    Write new test: Initial entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -45,7 +45,7 @@ $(function() {
         });
     });
 
-    // the body has the class of menuhidden to make it hidden by default
+    // body has the class of menuhidden to make it hidden by default
     describe('The menu', function() {
         it('hidden menu', function() {
             expect($('.menu-hidden').is(':visible')).toBe(true);
@@ -63,26 +63,17 @@ $(function() {
     });
 
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+    describe('Initial Entries', function() {
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        it('entry element is present', function(){
+            expect($('.feed').length).not.toBe(0);
+        });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
 
     /* TODO: Write a new test suite named "New Feed Selection"
 
